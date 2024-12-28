eeb80653772aa8c39276525f7b92e7cb42be0b6f
MOHAMMED-SAEED-OMER <muhammadsahed55@gmail.com>
2024-12-28 23:44:23 +0300

    Create welcome_page.py

---
+++ pages/welcome_page.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def welcome_page():
+    st.title("Welcome to the E-Operation App")
+    st.markdown("""
+        - **Submit Requests:** Use the form to submit a new request.
+        - **View Database:** View all submitted requests.
+    """)
