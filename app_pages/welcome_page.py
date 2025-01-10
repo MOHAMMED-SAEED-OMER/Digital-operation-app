05e9bcf6486354bdb7e193d5145cd3c480c9b5ed
MOHAMMED-SAEED-OMER <muhammadsahed55@gmail.com>
2025-01-10 16:58:23 +0300

    Update welcome_page.py

--- app_pages/welcome_page.py
+++ app_pages/welcome_page.py
@@ -4,6 +4,6 @@ def welcome_page():
     st.title("Welcome to the E-Operation App")
     st.markdown("""
         - **Submit Requests:** Use the form to submit a new request.
-        - **View Database:** View all submitted requests.
+        - **View Database:** View all submitted requests..
     """)
 
