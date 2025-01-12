70a0584ce1c5e7e61e34a01162f724c4bc1e8065
MOHAMMED-SAEED-OMER <muhammadsahed55@gmail.com>
2025-01-12 10:16:24 +0300

    Update design.py

--- utils/design.py
+++ utils/design.py
@@ -61,15 +61,32 @@ def enhanced_navigation_bar():
     user_name = st.session_state['user_info']['name'].capitalize()
     st.sidebar.markdown(f"Welcome, **{user_name}**!", unsafe_allow_html=True)
 
+    # Description for Hasar Organization
+    st.sidebar.markdown("<hr>", unsafe_allow_html=True)
+    st.sidebar.markdown("""
+        <h3 style='text-align: center;'>Hasar Organization</h3>
+        <p style='text-align: center; color: #555;'>For Climate Action and Sustainability</p>
+    """, unsafe_allow_html=True)
+
     # Useful Links
     st.sidebar.markdown("<hr>", unsafe_allow_html=True)
     st.sidebar.markdown("<h3 style='text-align: center;'>Quick Links</h3>", unsafe_allow_html=True)
     st.sidebar.markdown("""
         <ul style="list-style-type: none; padding-left: 0;">
-            <li><a href="https://www.google.com" target="_blank" style="text-decoration: none;">Google</a></li>
-            <li><a href="https://www.streamlit.io" target="_blank" style="text-decoration: none;">Streamlit Docs</a></li>
-            <li><a href="https://github.com" target="_blank" style="text-decoration: none;">GitHub</a></li>
+            <li><a href="https://hasar.org/" target="_blank" style="text-decoration: none;">🌐 Visit Our Website</a></li>
+            <li><a href="https://www.facebook.com/Hasar.org" target="_blank" style="text-decoration: none;">📘 Facebook</a></li>
+            <li><a href="https://www.instagram.com/hasarorganization/" target="_blank" style="text-decoration: none;">📸 Instagram</a></li>
         </ul>
+    """, unsafe_allow_html=True)
+
+    # Contact Support
+    st.sidebar.markdown("<hr>", unsafe_allow_html=True)
+    st.sidebar.markdown("<h3 style='text-align: center;'>Contact Us</h3>", unsafe_allow_html=True)
+    st.sidebar.markdown("""
+        <div class="center-text">
+            📧 Email: <a href="mailto:support@yourapp.com">support@hasar.org</a><br>
+            📞 Phone: +9647507810474
+        </div>
     """, unsafe_allow_html=True)
 
     # Logout Button
@@ -77,20 +94,11 @@ def enhanced_navigation_bar():
         st.session_state["user_info"] = None
         st.experimental_rerun()
 
-    # Add more functionality
-    st.sidebar.markdown("<hr>", unsafe_allow_html=True)
-    st.sidebar.markdown("<h3 style='text-align: center;'>Contact Us</h3>", unsafe_allow_html=True)
-    st.sidebar.markdown("""
-        <div class="center-text">
-            📧 Email: <a href="mailto:support@yourapp.com">support@yourapp.com</a><br>
-            📞 Phone: +1-234-567-890
-        </div>
-    """, unsafe_allow_html=True)
-
     # Footer
     st.sidebar.markdown("<hr>", unsafe_allow_html=True)
     st.sidebar.markdown("""
         <div class="center-text" style="font-size: 12px;">
-            &copy; 2025 Your Company. All rights reserved.
+            &copy; 2025 Hasar Organization. All rights reserved.
         </div>
     """, unsafe_allow_html=True)
+
